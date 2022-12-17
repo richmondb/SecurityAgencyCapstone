1b4d77a05ce22af1f3e9821824f3ee0a5eafaf31
richmondb <richmond.billones@gmail.com>
2022-12-17 22:32:32 +0800

    added support for Particles & code cleanup

--- main/settings.py
+++ main/settings.py
@@ -146,3 +146,12 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/home/"
+
+
+# When trying to host to the internet this configures it to be allowable
+# can be remove later on
+ALLOWED_HOSTS = ['*','9953-112-200-100-125.ap.ngrok.io']
+
+# csrf token is strict to we add the wesbite Origin 
+# can be remove later on
+CSRF_TRUSTED_ORIGINS = ['https://9953-112-200-100-125.ap.ngrok.io']
