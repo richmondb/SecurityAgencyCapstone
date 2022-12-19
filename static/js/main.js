ee6d35f1c6ca49cbfedf4f27e7dfd2d614770d9b
richmondb <richmond.billones@gmail.com>
2022-12-19 18:24:05 +0800

    make navbar transparent

--- static/js/main.js
+++ static/js/main.js
@@ -4,21 +4,22 @@ var video = document.querySelectorAll('video');
 video.forEach(element => {
     // Now do something with my button
     element.setAttribute('oncontextmenu', "return false;");
+    element.disablePictureInPicture = true;
 });
 
 
-// get div name
-let box = document.querySelector("header");
-// get element by id
-let e = document.getElementById("sec_1");
-// get div width and height
-// let width = box.offsetWidth;
-let width = screen.width;
-let height = box.offsetHeight;
-// set height via js
-if (width > 992) {
-    e.style.height = "calc(100vh - " + height + "px)";
-}
-//  remove for production
-console.log({ height });
-console.log({ width });
+// // get div name
+// let box = document.querySelector("header");
+// // get element by id
+// let e = document.getElementById("sec_1");
+// // get div width and height
+// // let width = box.offsetWidth;
+// let width = screen.width;
+// let height = box.offsetHeight;
+// // set height via js
+// if (width > 992) {
+//     e.style.height = "calc(100vh - " + height + "px)";
+// }
+// //  remove for production
+// console.log({ height });
+// console.log({ width });
